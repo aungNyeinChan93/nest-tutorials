8e1c1c4a297baa26eee9e081b0b27864335bb18f
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-12-07 23:36:26 +0630

    use typeOrm transacation

--- tuto_02/src/users/users.service.ts
+++ tuto_02/src/users/users.service.ts
@@ -3,23 +3,38 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { EntityManager, Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { UserProfile } from './entities/userProfile.entity';
-import { NotFoundError } from 'rxjs';
+import { Post } from 'src/posts/entities/post.entity';
 
 @Injectable()
 export class UsersService {
 
   constructor(
     @InjectRepository(User) private readonly userRepo: Repository<User>,
-    @InjectRepository(UserProfile) private readonly userProfileRepo: Repository<UserProfile>
+    @InjectRepository(UserProfile) private readonly userProfileRepo: Repository<UserProfile>,
+    private entityManager: EntityManager,
   ) { }
 
   async create(createUserDto: CreateUserDto) {
-    const user = await this.userRepo.save(createUserDto);
-    await this.userProfileRepo.save({ ...createUserDto?.userProfile, user })
-    return user;
+    const { post, user, userProfile } = await this.entityManager.transaction(async (entityManager) => {
+      const user = await this.userRepo.save(createUserDto);
+      const userProfile = this.userProfileRepo.create({ ...createUserDto?.userProfile, user })
+      await entityManager.save(userProfile)
+
+      // test 
+      const post = new Post();
+      post.title = 'test';
+      post.user = user;
+      post.description = '';
+      await entityManager.save(post)
+      // test end
+
+
+      return { user, userProfile, post };
+    })
+    return { user, userProfile, post };
   }
 
   async findAll() {
@@ -30,6 +45,7 @@ export class UsersService {
   async findOne(id: number) {
     const user = await this.userRepo.findOne({ where: { id } })
     if (!user) throw new NotFoundException('user not found!')
+
     return user;
   }
 
